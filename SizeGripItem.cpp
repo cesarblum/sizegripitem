437e070b17ab3f9bc1373a1b379f928b70d8e311
Kuba Ober <kuba@mareimbrium.org>
2016-11-02 13:55:28 -0400

    Mark unused parameters.

--- SizeGripItem.cpp
+++ SizeGripItem.cpp
@@ -141,6 +141,9 @@ void SizeGripItem::paint(QPainter* painter,
                          const QStyleOptionGraphicsItem* option,
                          QWidget* widget)
 {
+    Q_UNUSED(painter);
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 }
 
 #define IMPL_SET_FN(TYPE, POS)                  \
